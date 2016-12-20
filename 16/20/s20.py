b9b048d0f1c60daee6db0afdcba4365d3021f28c
MarkNOakden <mark@oakden.org>
2016-12-20 13:09:48 +0000

    bugfixes, comments and notebook prettification

--- 16/20/s20.py
+++ 16/20/s20.py
@@ -2,11 +2,14 @@
 import pandas as pd
 import numpy as np
 
+MAXINT = 4294967295
+
 def first_gap(r):
     i = 1
     ls, le = r[0]
     while r[i][0] <= le + 1:
-        ls, le = r[i]
+        if r[i][1] > le:
+            le = r[i][1]
         i += 1
     return le + 1
 
@@ -14,14 +17,25 @@ if __name__ == '__main__':
     fwrules = np.loadtxt('input.txt', delimiter='-', dtype=np.uint32)
     fwrules.sort(axis=0)
     
-    print 'Part1: first allowed IP is {}'.format(first_gap(fwrules))
+    print 'Part1: (via firstgap() function on numpy array)'
+    print '       first allowed IP is {}'.format(first_gap(fwrules))
 
-    fwdf = pd.DataFrame(fwrules)
-    fwdf['ipcount'] = fwdf[0] - fwdf[1].shift(1) - 1
+    fwdf = pd.DataFrame(fwrules, columns=['start', 'end'])
+    fwdf['maxEnd'] = fwdf.cummax()['end']
+    fwdf['ipcount'] = fwdf['start'] - fwdf['maxEnd'].shift(1) - 1
+    fwdf.loc[fwdf['ipcount'] < 0, 'ipcount'] = 0
+    fwdf['firstip'] = (fwdf[fwdf['ipcount'] > 0]['start']
+                       - fwdf['ipcount'])
+    fwdf = fwdf.fillna(0).astype(np.uint32)
 
-    allowedIPCount = int(np.sum(fwdf[
-        fwdf['ipcount'] > 0
-        ]['ipcount'].dropna()))
+    print 'Part1: (via pandas only)'
+    print '       first allowed IP is {}'.format(
+        fwdf[fwdf['firstip'] > 0]['firstip'].iloc[0])
+        
+    allowedIPCount = np.sum(fwdf['ipcount'])
+    # just in case the last range in the input leaves addresses open
+    # above it
+    allowedIPCount += MAXINT - fwdf['maxEnd'].iloc[-1]
 
     print 'Part2: number of allowed IPs is {}'.format(allowedIPCount)
     
