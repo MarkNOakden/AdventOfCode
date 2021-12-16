219bb2e23c3811d2190c2e69cff090a0e03be0ac
Mark Oakden <mark.oakden@virtualclarity.com>
2021-12-16 12:16:52 +0000

    day01 tidyup: put initialisation into __main__ clause, rename a variable to not shadow global scope

--- 21/01/day01.py
+++ 21/01/day01.py
@@ -3,18 +3,11 @@
 import pandas as pd
 import numpy as np
 
-depths = pd.read_csv(
-    'input.txt',
-    header=None,
-    names=['depth'],
-    dtype=int,
-)
-
 
 def part1(df):
-    depths = df['depth']
+    _depths = df['depth']
     next_depths = df['depth'].shift(-1)
-    return np.sum(next_depths > depths)
+    return np.sum(next_depths > _depths)
 
 
 def part2(df, window_length=3):
@@ -23,5 +16,12 @@ def part2(df, window_length=3):
 
 
 if __name__ == '__main__':
+    depths = pd.read_csv(
+        'input.txt',
+        header=None,
+        names=['depth'],
+        dtype=int,
+    )
+
     print('Part1: ', part1(depths))
     print('Part2: ', part2(depths))
