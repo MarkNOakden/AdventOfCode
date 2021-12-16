a7aabee1b6ebec7f420de9f0a2d0d8b6f5bcd7f9
Mark Oakden <mark.oakden@virtualclarity.com>
2021-12-16 11:37:05 +0000

    create skeleton files for new day

---
+++ 21/skeleton/dayNN.py
@@ -0,0 +1,13 @@
+"""Advent of Code 2021 day NN"""
+
+
+def part1(*args):
+    pass
+
+def part2(*args):
+    pass
+
+
+if __name__ == '__main__':
+    print('Part1: ', part1(depths))
+    print('Part2: ', part2(depths))
---
+++ 21/skeleton/test_dayNN.py
@@ -0,0 +1,22 @@
+"""Tests for Advent of Code 2021 day 01"""
+
+import unittest
+from dayNN import part1, part2
+
+test_data = """"""
+
+part1_01_result = None
+part2_01_result = None
+
+
+class TestDayNN(unittest.TestCase):
+
+    def test_part1_01(self):
+        self.assertEqual(part1(test_data), part1_01_result)
+
+    def test_part2_01(self):
+        self.assertEqual(part2(test_data), part2_01_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
